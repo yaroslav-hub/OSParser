61fa02b96da044a77d6ccf360bcbb3b7fbd44b77
Yaroslav <pakhmutovyaroslav@gmail.com>
2022-12-22 12:58:46 +0300

    Finish

--- ll/RecursiveDescent/RecursiveDescent/RecursiveDescent.cs
+++ ll/RecursiveDescent/RecursiveDescent/RecursiveDescent.cs
@@ -2,49 +2,83 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RecursiveDescent
 {
     public class RecursiveDescent
     {
-        public void Check()
+        private readonly string _code;
+        private int _currentReadIndex;
+        private readonly List<string> _lexems;
+        public RecursiveDescent(string code)
         {
-            ParseProg();
+            _code = code;
+            _currentReadIndex = 0;
+            _lexems = _code.Split(" ").ToList();
         }
 
-        public void ParseProg()
+        private string GetLexem()
         {
-            if (GetLexem().Equals("PROG"))
+            if (_currentReadIndex == _lexems.Count())
             {
-                if (GetLexem.Equals("id"))
-                {
-                    ParseVar();                    
-                    if (GetLexem.Equals("begin"))
-                    {
-                        ParseListst();
-                        if (GetLexem.Equals("end"))
-                        {
-                            bool end = false;
-                            try
-                            {
-                                GetLexem();
-                            }
-                            catch (EndOfStreamException e)
-                            {
-                                end = true;
-                            }
-                            if (!end)
-                            {
-                                throw new ApplicationException("Symbol after \"end\"");
-                            }
-                            return;
-                        }
-                    }
-                }
+                throw new EndOfStreamException();
             }
-            throw new ApplicationException("Error on parsing program structure");
+
+            while (_lexems[_currentReadIndex] == "")
+            {
+                _currentReadIndex++;
+            }
+
+            return _lexems[_currentReadIndex++];
+        }
+
+        private void CheckWrite()
+        {
+            if (GetLexem() != "WRITE" || GetLexem() != "(")
+            {
+                throw new Exception("'WRITE(' does not exists in");
+            }
+
+            try
+            {
+                CheckIdList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            if (GetLexem() != ")" || GetLexem() != ";")
+            {
+                throw new Exception("');' does not exists in");
+            }
+        }
+
+        private void CheckRead()
+        {
+            if (GetLexem() != "READ" || GetLexem() != "(")
+            {
+                throw new Exception("'READ(' does not exists in");
+            }
+
+            try
+            {
+                CheckIdList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            if (GetLexem() != ")" || GetLexem() != ";")
+            {
+                throw new Exception("');' does not exists in");
+            }
+        }
+
+        public bool Check()
+        {
+            ParseProg();
         }
 
         private void ParseVar()
