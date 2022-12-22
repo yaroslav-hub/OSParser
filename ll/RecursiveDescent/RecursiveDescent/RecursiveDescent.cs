aa80bfa80f9b1161c3036fd752379827f1d0f5c8
Aleksey Pavlovskiy <aleksey.pavlovskiy02@mail.ru>
2022-12-22 12:28:50 +0300

    1

--- ll/RecursiveDescent/RecursiveDescent/RecursiveDescent.cs
+++ ll/RecursiveDescent/RecursiveDescent/RecursiveDescent.cs
@@ -8,9 +8,26 @@ namespace RecursiveDescent
 {
     public class RecursiveDescent
     {
-        public bool Check(string code)
+        public bool Check()
         {
 
+        }
+
+        public bool ParseProg()
+        {
+            string[] codeParts = code.Split(' ');
+            if (codeParts[0].Equals("PROG"))
+            {
+                if (codeParts[1].Equals("id"))
+                {
+                    string varPart = "";
+                    int pos = 2;
+                    while (!codeParts[pos] != "begin")
+                    {
+                        if (pos)
+                    }
+                }
+            }
         }
     }
 }
