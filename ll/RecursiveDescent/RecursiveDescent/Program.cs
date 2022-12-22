6ea7f605fc1aa2cb5dea979cf0ebdc28c6b3b510
Aleksey Pavlovskiy <aleksey.pavlovskiy02@mail.ru>
2022-12-22 12:00:08 +0300

    init

---
+++ ll/RecursiveDescent/RecursiveDescent/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace RecursiveDescent
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string file = Console.ReadLine();
+            StreamReader fileStream = new StreamReader(file);
+            string code = fileStream.ReadLine();
+            RecursiveDescent recursiveDescent = new RecursiveDescent();
+            try
+            {
+                bool success = recursiveDescent.Check(code);
+                Console.WriteLine($"Parsing success: {success}");
+            }
+            catch (ApplicationException e)
+            {
+                Console.WriteLine($"Parsing error: {e}");
+            }
+        }
+    }
+}
---
+++ ll/RecursiveDescent/RecursiveDescent/RecursiveDescent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveDescent
+{
+    public class RecursiveDescent
+    {
+        public bool Check(string code)
+        {
+
+        }
+    }
+}
