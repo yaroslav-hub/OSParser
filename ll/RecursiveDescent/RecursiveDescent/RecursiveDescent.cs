f1f6765d087d7a79ca6b27b0b15321840a2c2a1e
Grishw <57558364+Grishw@users.noreply.github.com>
2022-12-22 20:18:01 +0300

    add error exception in idlist

--- ll/RecursiveDescent/RecursiveDescent/RecursiveDescent.cs
+++ ll/RecursiveDescent/RecursiveDescent/RecursiveDescent.cs
@@ -8,9 +8,106 @@ namespace RecursiveDescent
 {
     public class RecursiveDescent
     {
-        public bool Check(string code)
-        {
 
+        // GetLexem() -> string
+
+
+        string GetLexem()
+        { return ""; }
+        void CheckRead()
+        { }
+        void CheckWrite()
+        { }
+        void CheckAssign()
+        { }
+
+        void CheckIDLIST()
+        {
+            try
+            {
+                bool resultFlag = RecursIDLIST(GetLexem());
+                if(!resultFlag)
+                {
+                    throw new ArgumentException("id list Error: waited id list");
+                }
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
         }
+
+        bool RecursIDLIST(string str)
+        {
+            bool resultFlag = false;
+
+            switch (str)
+            {
+                case "id,":
+                    resultFlag = RecursIDLIST(GetLexem());
+                    break;
+                case "id":
+                    return true;
+                default:
+                    throw new ArgumentException("id Error: waited id");
+            }
+
+            return resultFlag;
+        }
+
+
+        void CheckLISTST()
+        {
+            try
+            {
+                RecursLISTST();
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+        }
+
+        void RecursLISTST()
+        {
+            try
+            {
+                CheckST();
+                RecursLISTST();
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+        }
+
+        void CheckST()
+        {
+            string str = GetLexem();
+            try
+            {
+                switch (str)
+                {
+                    case "READ":
+                        CheckRead();
+                        break;
+                    case "WRITE":
+                        CheckWrite();
+                        break;
+                    case "id":
+                        CheckAssign();
+                        break;
+                    default:
+                        throw new ArgumentException("ST Error");
+                }
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+            
+
+        }  
+
     }
 }
